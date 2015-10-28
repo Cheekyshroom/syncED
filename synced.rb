d18d8a853a3ac1160bdcf3e3f8f97cb2708374e5
cheekyshroom <phernima@ucsc.edu>
2015-10-28 00:40:13 -0700

    Default config and loading from config supported.

--- synced.rb
+++ synced.rb
@@ -2,12 +2,50 @@
 require "pstore"
 require "socket"
 
+class Cfg
+   @@default_name = "data/config.pstore"
+   def initialize(filename=nil)
+      if filename then
+         @data = PStore.new(filename)
+      else
+         @data = PStore.new(@@default_name)
+         @data.transaction do 
+            @data["users"] = []
+            @data["port"] = 12345
+            @data["name"] = "Unnamed server!"
+         end
+      end
+   end
+   def get(name)
+      @data.transaction { @data[name] }
+   end
+end
+
+class Server
+   def initialize(config)
+      @config = config
+      @connections = []
+      @socket = TCPServer.new(@config.get("port"))
+      c = @socket.accept
+      c.puts("Hello")
+      loop do
+         i = c.gets.chomp
+         c.puts(@config.get(i))
+         break if i == "quit"
+      end
+      c.close
+   end
+end
+
 def run_client
-   puts("Client.")
+
 end
 
 def run_server
-   puts("Server.")
+   config_exists = Dir.exist?("data")
+   Dir.mkdir("data") unless config_exists
+   config = config_exists ? Cfg.new("data/config.pstore") : Cfg.new
+   server = Server.new(config)
 end
 
 $type, = ARGV
