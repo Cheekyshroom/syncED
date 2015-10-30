7339fd45eb263e32dd9abe2066f7868e08fa9a42
cheekyshroom <phernima@ucsc.edu>
2015-10-29 23:44:23 -0700

    Offloaded config stuff to another file.

---
+++ cfg.rb
@@ -0,0 +1,20 @@
+require "pstore"
+
+class Cfg
+   @@default_name = "data/config.pstore"
+   def initialize(filename=nil)
+      if filename
+         @data = PStore.new(filename)
+      else
+         @data = PStore.new(@@default_name)
+         @data.transaction do 
+            @data["users"] = []
+            @data["port"] = 12345
+            @data["name"] = "Unnamed server!"
+         end
+      end
+   end
+   def get(name)
+      @data.transaction { @data[name] }
+   end
+end
